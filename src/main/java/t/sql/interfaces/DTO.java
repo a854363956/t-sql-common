e0c58b709a6a3c23a5bac4e392de226553db7bdb
zhangj <zhangjin0908@hotmail.com>
2018-06-20 11:38:25 +0800

    添加序列化接口

--- src/main/java/t/sql/interfaces/DTO.java
+++ src/main/java/t/sql/interfaces/DTO.java
@@ -1,9 +1,12 @@
 package t.sql.interfaces;
+
+import java.io.Serializable;
+
 /**
  * 用来标识当前实体为可以操作的DTO对象
  * @author zhangj
  * @date   2018-05-20 09:20:43
  * @email  zhangjin0908@hotmail.com
  */
-public interface DTO {
+public interface DTO extends Serializable {
 }
